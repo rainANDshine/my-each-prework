a8952579b11845b2f4c7a6141da4bb6e866978cc
Shun Yao <ysmiracle@hotmail.com>
2018-03-28 23:10:37 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,5 +1,4 @@
 def my_each(collection)
-  if block_given?
     i = 0
   
     while i < collection.length
@@ -8,7 +7,4 @@ def my_each(collection)
     end
   
     collection
-  else
-    "This block should not run!"
-  end
 end
